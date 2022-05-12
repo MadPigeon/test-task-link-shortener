35d41e5c7bb3017d032d7f4ad59ff081c95c4e13
MadPigeon <vsevolod007@gmail.com>
2022-05-12 16:19:17 +0600

    initial commit basic class and gitignore are created, the project can be launched both from VS code and command line, a test runs

---
+++ src/main/java/com/yourcodereview/vsevolod_rychkov/link_shortener/App.java
@@ -0,0 +1,7 @@
+package com.yourcodereview.vsevolod_rychkov.link_shortener;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
---
+++ src/test/java/com/yourcodereview/vsevolod_rychkov/link_shortener/AppTest.java
@@ -0,0 +1,12 @@
+package com.yourcodereview.vsevolod_rychkov.link_shortener;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}

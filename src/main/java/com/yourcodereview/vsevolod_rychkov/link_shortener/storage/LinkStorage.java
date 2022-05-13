cf011471c3484abc3bac5839193090bc0f321b49
MadPigeon <vsevolod007@gmail.com>
2022-05-13 11:03:51 +0600

    reduced nesting made a method easier to read

--- src/main/java/com/yourcodereview/vsevolod_rychkov/link_shortener/storage/LinkStorage.java
+++ src/main/java/com/yourcodereview/vsevolod_rychkov/link_shortener/storage/LinkStorage.java
@@ -11,14 +11,12 @@ public class LinkStorage {
   private static BiMap<String, String> storage = HashBiMap.create();
 
   public static String shorten(String longLink) {
-    if (!storage.containsValue(longLink)) {
-      final String shortLink = LinkGenerator.createLink();
-      storage.put(shortLink, longLink);
-      return shortLink;
-    } else {
+    if (storage.containsValue(longLink)) {
       return storage.inverse().get(longLink);
     }
-
+    final String shortLink = LinkGenerator.createLink();
+    storage.put(shortLink, longLink);
+    return shortLink;
   }
 
   public static String get(String shortLink) {

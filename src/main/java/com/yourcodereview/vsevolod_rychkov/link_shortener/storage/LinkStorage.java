975f847528a6d8c7d83fc5322b66d9c685ad0d51
MadPigeon <vsevolod007@gmail.com>
2022-05-13 10:33:13 +0600

    created a link storage that currently uses bi-directional map for link storage

---
+++ src/main/java/com/yourcodereview/vsevolod_rychkov/link_shortener/storage/LinkStorage.java
@@ -0,0 +1,27 @@
+package com.yourcodereview.vsevolod_rychkov.link_shortener.storage;
+
+import com.google.common.collect.BiMap;
+import com.google.common.collect.HashBiMap;
+import com.yourcodereview.vsevolod_rychkov.link_shortener.generation.LinkGenerator;
+
+public class LinkStorage {
+  
+  private static BiMap<String, String> storage = HashBiMap.create();
+
+
+  public static String shorten(String longLink) {
+    if (!storage.containsValue(longLink)) {
+      final String shortLink = LinkGenerator.createLink();
+      storage.put(shortLink, longLink);
+      return shortLink;
+    } else {
+      return storage.inverse().get(longLink);
+    }
+
+  }
+
+  public static String get(String shortLink) {
+    return storage.get(shortLink);
+  }
+
+}

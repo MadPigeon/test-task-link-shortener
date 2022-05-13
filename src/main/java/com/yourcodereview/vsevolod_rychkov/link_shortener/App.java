468239893ef4a4bca39db4d32febe721aedf457c
MadPigeon <vsevolod007@gmail.com>
2022-05-13 20:50:30 +0600

    added spring application launch

--- src/main/java/com/yourcodereview/vsevolod_rychkov/link_shortener/App.java
+++ src/main/java/com/yourcodereview/vsevolod_rychkov/link_shortener/App.java
@@ -1,7 +1,11 @@
 package com.yourcodereview.vsevolod_rychkov.link_shortener;
 
+import com.yourcodereview.vsevolod_rychkov.link_shortener.spring.StartLinkApplication;
+
+import org.springframework.boot.SpringApplication;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    SpringApplication.run(StartLinkApplication.class, args);
   }
 }
--- src/main/java/com/yourcodereview/vsevolod_rychkov/link_shortener/spring/StartLinkApplication.java
+++ src/main/java/com/yourcodereview/vsevolod_rychkov/link_shortener/spring/StartLinkApplication.java
@@ -1,5 +1,8 @@
 package com.yourcodereview.vsevolod_rychkov.link_shortener.spring;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StartLinkApplication {
   
 }
